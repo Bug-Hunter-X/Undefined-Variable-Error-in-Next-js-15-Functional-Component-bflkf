38650c5553b8a0cefa896e4feaa59f97571e8b2b
BugHunter-X <askzenai@gmail.com>
2025-02-02 06:54:06 +0100

    Add aboutSolution.js file

---
+++ aboutSolution.js
@@ -0,0 +1,13 @@
+```javascript
+// pages/aboutSolution.js
+
+export default function About() {
+  const myVar = "Hello from About page"; // Define the variable
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>The value of myVar is: {myVar}</p>
+    </div>
+  );
+}
+```
